48f0748561cb116368dc129110a7c3a7be12171a
Irina <i.tsimpilova@gmail.com>
2020-02-26 22:58:21 +0100

    Auth Middleware JWT
    Register

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\Cors;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -19,6 +20,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \App\Http\Middleware\Cors::class
     ];
 
     /**
@@ -40,6 +42,7 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\Cors::class
         ],
     ];
 
@@ -61,6 +64,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'cors' => \App\Http\Middleware\Cors::class
     ];
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -13,27 +13,25 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::group(['prefix' => 'v1/auth','middleware'=> ['cors']], function(){
 
-Route::prefix('v1')->group(function () {
-    Route::prefix('auth')->group(function () {
+    // Below mention routes are public, user can access those without any restriction.
+    // Create New User
+    Route::post('register', 'AuthController@register');
 
-        // Below mention routes are public, user can access those without any restriction.
-        // Create New User
-        Route::post('register', 'AuthController@register');
+    // Login User
+    Route::post('login', 'AuthController@login');
 
-        // Login User
-        Route::post('login', 'AuthController@login');
+    // Refresh the JWT Token
+    Route::get('refresh', 'AuthController@refresh');
 
-        // Refresh the JWT Token
-        Route::get('refresh', 'AuthController@refresh');
+    // Below mention routes are available only for the authenticated users.
+    Route::middleware('auth:api')->group(function () {
+        // Get user info
+        Route::get('user', 'AuthController@user');
 
-        // Below mention routes are available only for the authenticated users.
-        Route::middleware('auth:api')->group(function () {
-            // Get user info
-            Route::get('user', 'AuthController@user');
-
-            // Logout user from application
-            Route::post('logout', 'AuthController@logout');
-        });
+        // Logout user from application
+        Route::post('logout', 'AuthController@logout');
     });
+
 });
